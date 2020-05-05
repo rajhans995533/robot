852dc1c57bcf374f795174147bfd032662cd1d31
rajhans995533 <rajhans995533@gmail.com>
2020-05-06 03:48:48 +0530

    final commit

--- src/main/java/com/tic/RobotApplication.java
+++ src/main/java/com/tic/RobotApplication.java
@@ -3,11 +3,28 @@ package com.tic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tic.robot.Robot;
+import com.tic.service.RobotUtility;
+
 @SpringBootApplication
 public class RobotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RobotApplication.class, args);
+		//SpringApplication.run(RobotApplication.class, args);
+		Robot r = new Robot();
+//		System.out.println(r.readBarcode("112225"));
+//		System.out.println(r.readBarcode("112225B"));
+		//r.pickWeight(1);
+//		r.walkForKm(2);
+//		r.walkForKm(1);
+//		System.out.println(r.getRemainingBattery());
+		//System.out.println(Robot.walk(5,1));
+//		System.out.println(Robot.walk(4,3));
+//		System.out.println(Robot.walk(2));
+//		System.out.println(Robot.walk(2,12));
+		System.out.println(RobotUtility.walk(3.5));
+		System.out.println(RobotUtility.walk(2,3));
+		System.out.println(RobotUtility.walk(2,12));
 	}
 
 }
--- src/test/java/com/tic/RobotApplicationTests.java
+++ src/test/java/com/tic/RobotApplicationTests.java
@@ -1,13 +1,88 @@
 package com.tic;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.tic.robot.Robot;
+
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class RobotApplicationTests {
-
+	
+	
 	@Test
 	void contextLoads() {
 	}
 
+
+	@Test
+	void walk5Km() {
+		Robot robot=new Robot();
+		robot.walkForKm(5);
+		assertEquals(robot.getRemainingBattery(),"0 %");
+		
+	}
+
+	
+	@Test
+	void walkLessthan5Km() {
+		Robot robot=new Robot();
+		robot.walkForKm(3);
+		assertEquals(robot.getRemainingBattery(),"40 %");
+		
+	}
+	
+	
+	
+	@Test
+	void walkMorehan0Km() {
+		Robot robot=new Robot();
+		robot.walkForKm(0);
+		assertEquals(robot.getRemainingBattery(),"100 %");
+		
+	}
+	
+	
+	@Test
+	void walkMorehan6Km() {
+
+	    PrintStream save_out=System.out;final ByteArrayOutputStream out = new ByteArrayOutputStream();System.setOut(new PrintStream(out));
+	    Robot robot=new Robot();
+		robot.walkForKm(6);
+	    assertEquals("Sorry my Batery is not capable to complete this distance\r\n", out.toString());
+
+	    System.setOut(save_out);
+	}
+	
+	
+	@Test
+	void walkLessthan2KmWith3Kg() {
+		Robot robot=new Robot();
+		robot.pickWeight(3);
+		robot.walkForKm(2);
+		assertEquals(robot.getRemainingBattery(),"54 %");
+		
+	}
+	
+	
+	@Test
+	void walkWithOverWeight() {
+
+	    PrintStream save_out=System.out;final ByteArrayOutputStream out = new ByteArrayOutputStream();System.setOut(new PrintStream(out));
+	    Robot robot=new Robot();
+		robot.pickWeight(12);
+		robot.walkForKm(2);
+	    assertEquals("Overweight\r\n", out.toString());
+
+	    System.setOut(save_out);
+	}
+	
+	
+	
 }
